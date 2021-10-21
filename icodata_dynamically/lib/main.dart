77d780cae7f499239e2405cfa3bab56d0f79b94a
Stalin Thomas <stalingalaxy@gmail.com>
2021-10-21 21:47:43 +0530

    ico data dynamically Change

---
+++ icodata_dynamically/lib/main.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_final_fields
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'Icon Data Dynamically Change'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _categories = [
+    Category(title: "Test 1", icon: Icons.airline_seat_flat),
+    Category(title: "Test 2", icon: Icons.airline_seat_flat_angled),
+    Category(title: "Test 3", icon: Icons.airline_seat_individual_suite),
+    Category(title: "Test 4", icon: Icons.airline_seat_legroom_extra)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: _categories.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _categories[index];
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const Category({required this.title, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+      ),
+    );
+  }
+}
