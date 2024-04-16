bc8652ff83e7d9acbf89b14d8f8fa19a95bf7ece
Stalin Thomas <stalingalaxy@gmail.com>
2024-04-16 15:57:39 +0530

    Json serialize

---
+++ jsonserializecomplex/lib/models/user.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      name: json['name'] as String,
+      phone: json['phone'] as String,
+      email: json['email'] as String,
+      subscription: json['subscription'] as bool,
+      address: Address.fromJson(json['address'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'name': instance.name,
+      'phone': instance.phone,
+      'email': instance.email,
+      'subscription': instance.subscription,
+      'address': instance.address?.toJson(),
+    };
