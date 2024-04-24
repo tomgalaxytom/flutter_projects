2e100cf5151b335e080eed96d2c8f7c1c5e28602
Stalin Thomas <stalingalaxy@gmail.com>
2024-04-24 14:11:27 +0530

    using bottom navigation bar:

---
+++ Dart_part_exercise/using_bottom_navigation_bar.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      // Title
+      title: "Using Tabs",
+      // Home
+      home: MyHome()));
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  MyHomeState createState() => MyHomeState();
+}
+
+// SingleTickerProviderStateMixin is used for animation
+class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  // Create a tab controller
+  TabController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the Tab Controller
+    controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the Tab Controller
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        // Title
+        title: Text("Using Bottom Navigation Bar"),
+        // Set the background color of the App Bar
+        backgroundColor: Colors.blue,
+      ),
+      // Set the TabBar view as the body of the Scaffold
+      body: TabBarView(
+        // Add tabs as widgets
+        children: <Widget>[FirstTab(), SecondTab(), ThirdTab()],
+        // set the controller
+        controller: controller,
+      ),
+      // Set the bottom navigation bar
+      bottomNavigationBar: Material(
+        // set the color of the bottom navigation bar
+        color: Colors.blue,
+        // set the tab bar as the child of bottom navigation bar
+        child: TabBar(
+          tabs: <Tab>[
+            Tab(
+              // set icon to the tab
+              icon: Icon(Icons.favorite),
+            ),
+            Tab(
+              icon: Icon(Icons.adb),
+            ),
+            Tab(
+              icon: Icon(Icons.airport_shuttle),
+            ),
+          ],
+          // setup the controller
+          controller: controller,
+        ),
+      ),
+    );
+  }
+}
+
+class FirstTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        child: Center(
+          child: Column(
+            // center the children
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.favorite,
+                size: 160.0,
+                color: Colors.white,
+              ),
+              Text(
+                "First Tab",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(
+        child: Center(
+          child: Column(
+            // center the children
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.adb,
+                size: 160.0,
+                color: Colors.white,
+              ),
+              Text(
+                "Second Tab",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Container(
+        child: Center(
+          child: Column(
+            // center the children
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.airport_shuttle,
+                size: 160.0,
+                color: Colors.white,
+              ),
+              Text(
+                "Third Tab",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
