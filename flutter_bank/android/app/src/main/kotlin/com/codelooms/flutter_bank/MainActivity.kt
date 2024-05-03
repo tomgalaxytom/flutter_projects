bff38ec617bd7612dec383eedc8a7b8d6c7866cb
Stalin Thomas <stalingalaxy@gmail.com>
2024-05-03 18:14:41 +0530

    flutter bank ui only added

---
+++ flutter_bank/android/app/src/main/kotlin/com/codelooms/flutter_bank/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codelooms.flutter_flutter_bank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_bank/lib/pages/Loading.dart
@@ -0,0 +1,56 @@
+// ignore: file_names
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'homepage.dart';
+
+class Loadpage extends StatefulWidget {
+  const Loadpage({super.key});
+
+  @override
+  State<Loadpage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Loadpage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 500), () {
+      Navigator.pushReplacement(
+          context,
+          PageTransition(
+              type: PageTransitionType.fade, child: const Homepage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF3D0175),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 60,
+              height: 60,
+              child: CircularProgressIndicator(
+                strokeWidth: 4,
+                color: Color.fromARGB(255, 255, 0, 191),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            const Text(
+              "Loading...",
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
